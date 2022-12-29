33b522e8b541ba274678bc82ee0bf58063c54eda
tedgregory <mail@tedgregory.ru>
2022-12-29 20:00:46 +0300

    feat: make digital pad, feractor

--- server/build/modules/test.js
+++
@@ -1,4 +0,0 @@
-export function testModule(name = '') {
-    console.log('name', name);
-}
-//# sourceMappingURL=test.js.map
--- server/src/index.ts
+++ server/src/index.ts
@@ -1,75 +1,88 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { Game1Type } from './models/game1.js';
+import { BOT_COMMANDS, BOT_GAME1_OPTIONS } from './options.js';
+import * as dotenv from 'dotenv';
 
-const token = '5673896181:AAGxGb7zqGclP3Y4TtjtIk9OdRCdhr-Si0w';
+dotenv.config();
+
+const token = process.env.TOKEN || '';
 
 const bot = new TelegramBot(token, { polling: true });
 
 let games: Game1Type[] = [];
 
+const startGame = async (id: number) => {
+  const game = games.find((g) => g.chatId === id);
+  if (game) {
+    await bot.sendMessage(game.chatId, `You're in the game, you're in the game... keep on playing`);
+  }
+  const number = Math.floor(Math.random() * 10);
+  games.push({ chatId: id, number });
+  await bot.sendMessage(id, `I picked a number 0 to 9, guess it!`, BOT_GAME1_OPTIONS);
+};
+
+const handleGame1 = async (msg: TelegramBot.CallbackQuery) => {
+  if (!msg || !msg.message) {
+    return;
+  }
+  const { data } = msg;
+  const { id: cid, username } = msg.message.chat;
+  const game = games.find((g) => g.chatId === cid);
+  if (game) {
+    const attempt = Number(data);
+    if (!Number.isNaN(attempt) && attempt / 9 <= 1) {
+      if (attempt === game.number) {
+        await bot.sendSticker(
+          cid,
+          'https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/3.webp'
+        );
+        await bot.sendMessage(game.chatId, `Yep, ${username}, it is ${game.number}`);
+        games = games.filter((g) => g.chatId !== cid);
+        return;
+      }
+      await bot.sendMessage(game.chatId, `Nope, ${username}, try again`, BOT_GAME1_OPTIONS);
+      return;
+    }
+    await bot.sendMessage(
+      game.chatId,
+      `You're in the game, ${username}, you're in the game... keep on playing`,
+      BOT_GAME1_OPTIONS
+    );
+    return;
+  }
+};
+
 const start = async () => {
-  bot.setMyCommands([
-    { command: '/start', description: 'Start working' },
-    { command: '/info', description: 'About and options' },
-    { command: '/game', description: 'Play stupid game' },
-  ]);
+  bot.setMyCommands(BOT_COMMANDS);
 
   bot.on('message', async (msg) => {
     const { text } = msg;
     const { id, username, first_name } = msg.chat;
-    const game = games.find((g) => g.chatId === id);
-    if (game) {
-      const attempt = Number(text);
-      if (!Number.isNaN(attempt) && attempt / 9 <= 1) {
-        if (attempt === game.number) {
-          await bot.sendSticker(
-            id,
-            'https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/3.webp'
-          );
-          await bot.sendMessage(game.chatId, `Yep, ${username}, it is ${game.number}`);
-          games = games.filter((g) => g.chatId !== id);
-          return;
-        }
-        await bot.sendMessage(game.chatId, `Nope, ${username}, try again`);
-        return;
-      }
-      await bot.sendMessage(
-        game.chatId,
-        `You're in the game, ${username}, you're in the game... keep on playing`
-      );
-      return;
-    }
-    console.log(msg);
+
     switch (text) {
       case '/start': {
-        await bot.sendMessage(id, `Hello, ${first_name}!`);
+        bot.sendMessage(id, `Hello, ${first_name}!`);
         break;
       }
       case '/info': {
-        await bot.sendSticker(
+        bot.sendSticker(
           id,
           'https://tlgrm.eu/_/stickers/8a1/9aa/8a19aab4-98c0-37cb-a3d4-491cb94d7e12/1.webp'
         );
-        await bot.sendMessage(id, `This is a test bot playground, ${username}`);
+        bot.sendMessage(id, `This is a test bot playground, ${username}`);
         break;
       }
       case '/game': {
-        const game = games.find((g) => g.chatId === id);
-        if (game) {
-          await bot.sendMessage(
-            game.chatId,
-            `You're in the game, ${username}, you're in the game... keep on playing`
-          );
-          break;
-        }
-        const number = Math.floor(Math.random() * 10);
-        games.push({ chatId: id, number });
-        await bot.sendMessage(id, `I picked a number 0 to 9, ${username}, guess it!`);
+        await startGame(id);
       }
       default: {
-        await bot.sendMessage(id, 'Wtf man! I aint gecha!');
+        return;
       }
     }
+  });
+
+  bot.on('callback_query', async (msg) => {
+    return handleGame1(msg);
   });
 };
 
